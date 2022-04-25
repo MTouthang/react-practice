d936b75669b3252ab7ae64902edecec877157650
Mamang touthang <huebart6@gmail.com>
2022-04-25 14:38:52 +0530

    useState Hooks

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
+import { useState } from "react";
 import "./App.css";
 
-const Person = (props) => {
-  return (
-    <>
-      <h1> Name: {props.name}</h1>
-      <h1> Last: {props.last}</h1>
-      <h1> Age: {props.age}</h1>
-    </>
-  );
-};
-
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
-      <Person name="John" last="stev" age={20} />
-      <Person name="marcy" last="zou" age={33} />
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>+</button>
+      <h1>{count}</h1>
+      <button onClick={() => setCount((prevCount) => prevCount - 1)}>
+        {" "}
+        -{" "}
+      </button>
     </div>
   );
 }
